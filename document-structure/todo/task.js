5c883af4673f1949ed963ac9cfca3568425bbdd3
Vladimir Khleborodov <khleborodov@hotmail.com>
2023-12-10 21:36:48 +0300

    todo edit 1

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,3 +1,4 @@
+const form = document.getElementById('tasks__form');
 const button = document.getElementById('tasks__add');
 
 function createTask() {
@@ -6,7 +7,7 @@ function createTask() {
   const taskInput = document.getElementById('task__input');
   const taskInputValue = taskInput.value.trim();
   taskTitle.innerText = taskInputValue;
-
+  
   const hyperlink = document.createElement('a');
   hyperlink.classList.add('task__remove');
   hyperlink.setAttribute('href', '#');
@@ -14,16 +15,23 @@ function createTask() {
   hyperlink.addEventListener('click', () => {
     task.remove();
   });
-
+  
   const task = document.createElement('div');
   task.classList.add('task');
   task.appendChild(taskTitle);
   task.appendChild(hyperlink);
-
+  
   const tasksList = document.getElementById('tasks__list');
-  tasksList.appendChild(task);
+  if (taskInputValue === '') {
+    form.reset();
+    return;
+  } else {
+    tasksList.appendChild(task);
+    form.reset();
+  }
 }
 
-button.addEventListener('click', () => {
+button.addEventListener('click', (event) => {
+  event.preventDefault();
   createTask();
 });
