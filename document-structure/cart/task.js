949d7242e3aeebf29cd91882dfc8375e58e157db
Vladimir Khleborodov <khleborodov@hotmail.com>
2023-12-10 21:37:18 +0300

    cart edit 1

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -12,52 +12,51 @@ quantityControl.forEach((element, index) => {
     if (element.classList.contains('product__quantity-control_inc')) {
       productQuantity[productIndex].textContent = Number(productQuantity[productIndex].textContent) + 1;
     }
-
+    
     if (element.classList.contains('product__quantity-control_dec') && (productQuantity[productIndex].textContent > 1)) {
       productQuantity[productIndex].textContent = Number(productQuantity[productIndex].textContent) - 1;
     }
   });
 });
 
-// Отдельный массив, состоящий из объектов с названием товара и количеством
-let productsSelected = [];
+// Отдельный массив, состоящий из выбранных объектов с названием товара и количеством
+let productsSelectedArray = [];
 
 buttonsAddProduct.forEach((button, index) => {
   button.addEventListener('click', () => {
     let productList = [...document.getElementsByClassName("cart__product")];
     let productSelectedTitle = productTitle[index].textContent;
     let productSelectedQuantity = Number(productQuantity[index].textContent);
-    let productPositionInCart = productsSelected.findIndex((elem, i) => {
-      return productsSelected[i].productSelectedTitle === productSelectedTitle;
-    });
+    let productSelected = productsSelectedArray.find(elem => elem.productSelectedTitle === productSelectedTitle);
     
     //Если товара нет, создаем новый
-    if (productPositionInCart < 0) {
-      productsSelected.push({ productSelectedTitle, productSelectedQuantity });   
-
+    if (!productSelected) {
+      productsSelectedArray.push({ productSelectedTitle, productSelectedQuantity });   
+      
       const productCart = document.createElement("div");
       productCart.classList.add("cart__product");
       productCart.dataset.id = productTitle[index].closest(".product").dataset.id;
-
+      
       const productCartImage = document.createElement('img');
       productCartImage.classList.add('cart__product-image');
       productCartImage.setAttribute('src', productImages[index].getAttribute("src"));
       productCart.appendChild(productCartImage);
-
+      
       const productCartCount = document.createElement('div');
       productCartCount.classList.add('cart__product-count');
       productCartCount.innerText = productSelectedQuantity;
       productCart.appendChild(productCartCount);
-
+      
       cartProducts.appendChild(productCart);
-
+      
     // Если товар уже существует в корзине, изменяем его количество 
     } else { 
-      productSelectedQuantity = productsSelected[productPositionInCart].productSelectedQuantity + productSelectedQuantity;
+      let productSelectedIndex = productsSelectedArray.indexOf(productSelected);
+      productSelectedQuantity = productsSelectedArray[productSelectedIndex].productSelectedQuantity + productSelectedQuantity;
       // Обновляем данные товара в массиве productsSelected
-      productsSelected.splice(productPositionInCart, 1, { productSelectedTitle, productSelectedQuantity });
+      productsSelectedArray.splice(productSelectedIndex, 1, { productSelectedTitle, productSelectedQuantity });
       // Обновляем количество в существующем товаре в productList
-      productList[productPositionInCart].firstChild.nextElementSibling.textContent = productSelectedQuantity;
+      productList[productSelectedIndex].firstChild.nextElementSibling.textContent = productSelectedQuantity;
     }
   });
 });
