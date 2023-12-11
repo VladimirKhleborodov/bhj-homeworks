edeb17ff25ba4913cffccec005b035d2e0eff3a8
Vladimir Khleborodov <khleborodov@hotmail.com>
2023-12-11 13:35:54 +0300

    tooltip edit 2

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,14 +4,14 @@ const hyperlinkArr = [...document.querySelectorAll('.has-tooltip')];
 hyperlinkArr.forEach(el => {
   const tooltipElement = document.createElement('span');
   tooltipElement.classList.add('tooltip');
-  tooltipElement.classList.add('tooltip_active');
   
   el.addEventListener('click', (event) => {
     event.preventDefault();
-    if ([...el.children].includes(tooltipElement)) {
+    if (el.nextElementSibling.contains(tooltipElement)) {
       tooltipElement.classList.toggle('tooltip_active');
     } else {
-      el.appendChild(tooltipElement);
+      el.insertAdjacentElement('afterend', tooltipElement);
+      tooltipElement.classList.add('tooltip_active');
       tooltipElement.innerText = el.getAttribute('title');
       tooltipElement.style.position = 'absolute';
       tooltipElement.style.left = `${el.getBoundingClientRect().x}px`;
